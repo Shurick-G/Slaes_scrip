8eca5d1d62f36fb6578b96f0a265d34705db97d3
Shurick-G <114661889+Shurick-G@users.noreply.github.com>
2022-10-28 13:14:14 +0500

    AddScripScheme

---
+++ callIntroduction.js
@@ -0,0 +1,48 @@
+
+const callIntroduction = {
+	questions1:{
+		text: "Добрый день. Текст первого вопроса",
+		answears: {
+			answear1: {
+				text:"Текст первого ответа",
+				nextQuestions:_____
+				},
+			answear2: {
+					text:"Текст второго ответа",
+					nextQuestions:_____
+			},
+			answear3: {
+					text:"Текст тертьего ответа",
+					nextQuestions:_____
+	},
+	questions2:{
+		text: "Текст второго вопроса",
+		answears: {
+			answear1: {
+				text:"Текст первого ответа",
+				nextQuestions:_____
+				},
+			answear2: {
+					text:"Текст второго ответа",
+					nextQuestions:_____
+			},
+			answear3: {
+					text:"Текст тертьего ответа",
+					nextQuestions:_____
+	},
+	questions3:{
+		text: "Текст тертьего вопроса",
+		answears: {
+			answear1: {
+				text:"Текст первого ответа",
+				nextQuestions:_____
+				},
+			answear2: {
+					text:"Текст второго ответа",
+					nextQuestions:_____
+			},
+			answear3: {
+					text:"Текст тертьего ответа",
+					nextQuestions:_____
+	},
+}
